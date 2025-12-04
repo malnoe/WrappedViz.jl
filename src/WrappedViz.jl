20311da871d5d9a0a1ccfbd39fc3db7cc9df4f72
Garance Malnoë <106530489+malnoe@users.noreply.github.com>
2025-12-04 10:14:48 +0100

    init

--- src/WrappedViz.jl
+++ src/WrappedViz.jl
@@ -1,3 +1,16 @@
+# In case you want to know, why the last line of the docstring below looks like it is:
+# It will show the package (local) path when help on the package is invoked like     help?> WrappedViz
+# but it will interpolate to an empty string on CI server, 
+# preventing appearing the server local path in the documentation built there.
+
+"""
+    Package WrappedViz v$(pkgversion(WrappedViz))
+
+What did you listen to ?
+
+$(isnothing(get(ENV, "CI", nothing)) ? ("\n" * "Package local path: " * pathof(WrappedViz)) : "") 
+"""
+
 module WrappedViz
 
 # Write your package code here.
